8931c480be2f5ca6ccb4a017dc7e1c6eb93ff70f
shanmukha sahith <92036395+sahith2004@users.noreply.github.com>
2022-05-19 22:58:42 +0530

    This is a quiz app

---
+++ index.js
@@ -0,0 +1,100 @@
+const quizData = [
+    {
+        question: "Full form of IIT?",
+        a: "Indian Institute of TimePass",
+        b: "International Institute of TimePass",
+        c: "International Institute of Technology",
+        d: "Indian Institue of Technology",
+        correct: "d",
+    },
+    {
+        question: "Where is Kharaghpur located?",
+        a: "Karnataka",
+        b: "West Bengal",
+        c: "Telangana",
+        d: "Maharashtra",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Cascading Style Sheet",
+        c: "Jason Object Notation",
+        d: "Javascript",
+        correct: "a",
+    },
+    {
+        question: "Full form of KGP?",
+        a: "Kanpur",
+        b: "Kharaghpur",
+        c: "Karate",
+        d: "Kung-fu",
+        correct: "b",
+    },
+];
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
